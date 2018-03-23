a6d05e3459f219957ad77239f6e732e455cac372
Etienne Camenen <etienne.camenen@gmail.com>
2018-03-23 20:57:26 +0100

    Updated tests

--- runTest1.sh
+++ runTest1.sh
@@ -6,15 +6,28 @@
 #
 #Copyright: PhenoMeNal/INRA Toulouse 2017
 
-INFILE="data/sacurine_workflow_output.tsv"
+INFILE="reactions_recon2.02.tsv"
+SBML="recon2.02_without_compartment.xml"
 OUTFILE1="mapping.tsv"
 OUTFILE2="pathwayEnrichment.tsv"
 OUTFILE3="information.txt"
 ERRORS=""
 NBFAIL=0
 NBTEST=0
-TESTS=('' '-t 1' '-f 13' 'idSBML 2' '-inchi 4' '-inchi 4 -l c,h,p' '-inchi 4 -l p,h,c' '-l c,h,p' '-inchi 4 -l' '-l' '-chebi 3' '-inchikey 5' '-kegg 6' '-hmdb 8' '-csid 9' '-name -1' '-name -1 -inchi 4' '-s data/recon2.02.xml' '--header' '-sep \t')
-NAME_TESTS=('by default' 'with reactions' 'filtered column' 'id SBML mapping' 'inchi mapping' 'including p layer in mapping' 'with shuffled layer parameters' 'including p layer in mapping without inchi column' 'mapping on formula only' 'mapping on formula only without inchi column' 'ChEBI' 'InChIKey' 'KEGG' 'HMDB' 'CSID' 'without name mapping' 'without name mapping but with another mapping' 'with another SBML' 'without header' 'with separator argument')
+TESTS=('-t 2' '-t 4' '-t 5' ' -t 6 -idSBML 3' '-t 5 -tEnr 6' '-t 4 -tEnr 6' '-t 6 -idSBML 3 -tEnr 5' '-t 1' ''
+ '-tEnr 2' '-f 13' '-idSBML 2' '-chebi 3' '-inchi 4' '-inchi 4 -l c,h,t' '-inchi 4 -l t,h,c' '-l c,h,t'
+ '-inchi 4 -l' '-l' '-inchikey 5' '-kegg 6' '-pubchem 7' '-hmdb 8' '-csid 9' '-mass 12 -prec 2' '-prec 2'
+ '-name 1 -nameCol 2' '-nameCol -1 -name 1' '-s data/recon2.02_without_compartment.xml' '--header' '-sep \t'
+ '-sepID ;' '-inchi 4 -l c,h,t -lWarn -o1 temp/checking_format.tsv' '-inchi 4 -o1 temp/checking_format.tsv'
+  '-noCheck -o1 temp/checking_format2.tsv')
+NAME_TESTS=('reactions mapping' 'enzyme mapping' 'protein mapping' 'gene mapping' 'protein mapping & gene enrichment'
+'enzyme mapping & gene enrichment' 'gene mapping & protein enrichment' 'metabolite mapping' 'by default'
+'reaction enrichment' 'filtered column' 'id SBML mapping' 'ChEBI mapping' 'inchi mapping' 'inchi mapping, t layer'
+'inchi mapping, shuffled layers' 'layers without inchi mapping' 'mapping on formula only' 'on formula only without inchi column'
+'InChIKey' 'KEGG' 'HMDB' 'PubChem' 'ChemSpider' 'isotopic mass, prec 2' 'precision 2 without mass column'
+'name mapping with name column different' 'name column disabled, name mapping' 'recon flat sbml'
+'without header' 'tabulation separator' 'identifier separator' 'inchi, layers, layer warning, check file'
+'inchi mapping, check file', 'nocheking, check file')
 echo '' > resultRuns.log
 
 testError(){
@@ -60,7 +73,6 @@ testFileExist(){
 }
 
 printError(){
-    let NBTEST+=1
     testError $1 $2
     if [ $? -ne 0 ]; then
         echo -n "E"
@@ -72,21 +84,29 @@ printError(){
 run(){
     #java -jar pathwayEnrichment.jar -gal $@
     java -jar pathwayEnrichment.jar -gal $OUTFILE3 $@ >> resultRuns.log 2>&1
+    let NBTEST+=1
 }
 
 tests(){
     #local GIT_PATH="https://raw.githubusercontent.com/phnmnl/container-pathwayEnrichment/master/testData"
     for i in `seq 0 $((${#TESTS[@]} -1))`; do
-        #wget -q -P temp/ $GIT_PATH/mapping.tsv"${i}" $GIT_PATH/pathwayEnrichment.tsv"${i}" $GIT_PATH/info.txt"${i}"
-        run "-i $INFILE ${TESTS[i]}"
+        #wget -q -P temp/ $GIT_PATH/map/mapping.tsv"${i}" $GIT_PATH/enr/pathwayEnrichment.tsv"${i}" $GIT_PATH/info/information.txt"${i}"
+        if [[ $i -ge 1 ]] && [[ $i -le 6 ]]; then
+		    $INFILE="gpr_recon2.02.tsv"
+		    $SBML="recon2.02.xml"
+        elif [ $i -gt 6 ]; then
+            $INFILE="sacurine_workflow_output.tsv"
+            $SBML="recon2.02_without_compartment.xml"
+        fi
+        run "-i data/$INFILE -s data/$SBML ${TESTS[i]}"
         EXIT=$?
         printError $EXIT $i
     done
 }
 
 testsFail(){
-    TESTS=('-t 0' '-t 10' 'l xmlkfmrvgj' '-inchi 5' '-pubchem 7 -name -1' '-smiles 11 -name -1')
-    NAME_TESTS=('Wrong BioType' 'Wrong BioType bis' 'Wrong layers' 'Wrong column number: 0 mapping' 'PubChem' 'SMILES')
+    TESTS=('-t 0' '-t 10' 'l xmlkfmrvgj' '-inchi 5' '-smiles 11 -name -1')
+    NAME_TESTS=('Wrong BioType' 'Wrong BioType bis' 'Wrong layers' 'Wrong column number: 0 mapping' 'SMILES')
     for i in `seq 0 $((${#TESTS[@]} -1))`; do
         run "-i $INFILE ${TESTS[i]}"
         EXIT=$?
