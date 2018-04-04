f9987d523825f58ce01fd4adbfe4f678ac853591
Etienne Camenen <etienne.camenen@gmail.com>
2018-04-04 17:36:42 +0200

    TestsMapDB

--- runTest1.sh
+++ runTest1.sh
@@ -29,7 +29,7 @@ NBFAIL=0
 NBTEST=0
 PARAMETER=0
 EXIT=0
-TESTS=('')
+TESTS=()
 echo '' > resultRuns.log
 
 
@@ -41,9 +41,8 @@ testError(){
     local ACTUAL_NB_LINE_MAP=$( wc $OUTFILE1 | awk '{print $1}')
     local ACTUAL_NB_LINE_ENR=$( wc $OUTFILE2 | awk '{print $1}')
 
-    printf "$3"
-    [ $4 -ne $EXIT ] && {
-        MSG=$MSG"Program exited with error.\n"
+    [ $3 -ne $EXIT ] && {
+        MSG=$MSG"Program exited with bad error code: $3.\n"
         BOOLEAN_ERR="true"
     }
 
@@ -56,20 +55,27 @@ testError(){
     }
 
     ACTUAL_OUTPUT=$(cat $OUTFILE3 | tr '\n' ' ' )
-    if [[ $ACTUAL_OUTPUT != "$OUTPUT" ]]; then
-        MSG=$MSG"Expected warnings not found in output.\n"
-        printf "$OUTPUT"
-        printf "$ACTUAL_OUTPUT"
+    if [[ $ACTUAL_OUTPUT != *"$WARN"* ]]; then
+        MSG=$MSG"Expected warnings not found.\n"
+        echo "$WARN"
+        echo "$ACTUAL_OUTPUT"
+        BOOLEAN_ERR="true"
+    fi
+
+    if [[ $ACTUAL_OUTPUT != *"$OUTPUT"* ]]; then
+        MSG=$MSG"Expected output not found.\n"
+        echo "$OUTPUT"
+        echo "$ACTUAL_OUTPUT"
         BOOLEAN_ERR="true"
     fi
 
     [ $ACTUAL_NB_LINE_MAP -ne $1 ] && {
-        MSG=$MSG"Expected $1 lines in $OUTFILE1, Actual=$ACTUAL_NB_LINE_MAP lines.\n"
+        MSG=$MSG"Expected $1 lines in $OUTFILE1. Actual have $ACTUAL_NB_LINE_MAP lines.\n"
         BOOLEAN_ERR="true"
     }
 
     [ $ACTUAL_NB_LINE_ENR -ne $2 ] && {
-        MSG=$MSG"Expected $1 lines in $OUTFILE2 Actual=$ACTUAL_NB_LINE_ENR lines.\n"
+        MSG=$MSG"Expected $2 lines in $OUTFILE2. Actual have $ACTUAL_NB_LINE_ENR lines.\n"
         BOOLEAN_ERR="true"
     }
 
@@ -137,8 +143,12 @@ test(){
     for i in `seq 0 $((${#TESTS[@]} -1))`; do
         run "-i data/$INFILE -s data/$SBML ${TESTS[i]}"
         ACTUAL_EXIT=$?
-        printError $NB_LINE_MAP $NB_LINE_ENR $EXIT $i
-        #printError ${NB_LINE_MAP[i]} ${NB_LINE_ENR[i]} $ACTUAL_EXIT $i
+        if [ -z $1 ]; then
+            printError $NB_LINE_MAP $NB_LINE_ENR $ACTUAL_EXIT $i
+        else
+            OUTPUT=${OUTPUTS[i]}
+            printError ${NB_LINE_MAP[i]} ${NB_LINE_ENR[i]} $ACTUAL_EXIT $i
+        fi
     done
 }
 
@@ -148,17 +158,17 @@ testsDefault(){
     PARAMETER=0
     NAME='TEST_DEFAULT'
 
-    WARN=$MSG_DEF
-    OUTPUT="$WARN "$MSG_CHECK
-    setMapLogDefault "33" "30.0" "1.27"
-    setEnrLogDefault "39" "40.21"
-    #printf "$OUTPUT"
-
     TESTS=('-t 1')
     #TESTS=('' '-t 1' '-tEnr 3' '-s data/recon2.02_without_compartment.xml' '-sep \t' '-sepID ;')
     NB_LINE_MAP='111'
     NB_LINE_ENR='40'
-    EXIT=0
+
+    WARN=$MSG_DEF
+    OUTPUT=""
+    #OUTPUT="$WARN "$MSG_CHECK
+    setMapLogDefault "33" "30.0" "1.27"
+    setEnrLogDefault "39" "40.21"
+    #printf "$OUTPUT"
 
     test
 }
@@ -166,20 +176,25 @@ testsDefault(){
 testsMappingDB(){
     PARAMETER=0
 
-    TESTS=('-chebi 3' '-inchi 4' '-inchi 4 -l c,h,t' '-inchi 4 -l t,h,c' '-inchi 4 -l' '-inchikey 5' '-kegg 6' '-pubchem 7' '-hmdb 8' '-csid 9')
-    NB_LINE_MAP=('1' '1' '1' '1' '1' '1' '1' '1' '1' '1')
-    NB_LINE_ENR=('1' '1' '1' '1' '1' '1' '1' '1' '1' '1')
+    TESTS=('-chebi 3' '-inchi 4')
+    #TESTS=('-chebi 3' '-inchi 4' '-inchi 4 -l c,h,t' '-inchi 4 -l t,h,c' '-inchi 4 -l' '-inchikey 5' '-kegg 6' '-pubchem 7' '-hmdb 8' '-csid 9')
+    NB_LINE_MAP=('178' '116' '1' '1' '1' '1' '1' '1' '1' '1')
+    NB_LINE_ENR=('15' '39' '1' '1' '1' '1' '1' '1' '1' '1')
+    OUTPUTS=()
+    MAP_PAR=('14 12.73 0.54' '33 30.0 1.27' '4 3.64 0.15' '4 3.64 0.15' '45 40.91 1.74' '14 12.73 0.54' '12 10.91 0.46' '14 12.73 0.54' '13 11.82 0.5' '9 8.18 0.35')
+    ENR_PAR=('14 14.43' '38 39.18' '3 3.09' '3 3.09' '30 30.93' '15 15.46' '13 13.4' '14 14.43' '14 14.43' '10 10.31')
 
     WARN=$MSG_NAME
-    OUTPUT=="$WARN "$MSG_CHECK
+
     for i in `seq 0 $((${#TESTS[@]} -1))`; do
-        setMapLogDefault "33" "30.0" "1.27"
-        setEnrLogDefault "39" "40.21"
+        OUTPUT=""
+        setMapLogDefault ${MAP_PAR[i]}
+        #echo "$OUTPUT"
+        setEnrLogDefault ${ENR_PAR[i]}
+        OUTPUTS[i]="$OUTPUT"
     done
 
-
-
-    test
+    test "multiple"
 }
 
 
@@ -205,7 +220,8 @@ START_TIME=$(date -u -d $(date +"%H:%M:%S") +"%s")
 printf "Tests in progress, could take a few minutes...\n"
 #mkdir temp
 
-testsDefault
+#testsDefault
+testsMappingDB
 
 #rm -r temp/
 printf "\n$NBTEST tests, $NBFAIL failed.$ERRORS\n"
