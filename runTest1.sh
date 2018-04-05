f251ff8a11c7c5f6113a9b63c9a1a32313b0cad7
Etienne Camenen <etienne.camenen@gmail.com>
2018-04-05 17:36:22 +0200

    testName

--- runTest1.sh
+++ runTest1.sh
@@ -47,13 +47,14 @@ testError(){
     local OUTFILES=(${OUTFILE1} ${OUTFILE2} ${OUTFILE3})
     local ACTUAL_NB_LINE_MAP=$( wc ${OUTFILE1} | awk '{print $1}')
     local ACTUAL_NB_LINE_ENR=$( wc ${OUTFILE2} | awk '{print $1}')
+    local ACTUAL_OUTPUT=$(cat $OUTFILE3 | tr '\n' ' ' )
 
-    [ $3 -ne ${EXIT} ] && {
-        MSG=${MSG}"Program exited with bad error code: $3.\n"
+    [ $1 -ne ${EXIT} ] && {
+        MSG=${MSG}"Program exited with bad error code: $1.\n"
         BOOLEAN_ERR="true"
     }
 
-    [ $3 -eq 0 ] && [[ ( ! -f ${OUTFILE1} ) ||  ( ! -f ${OUTFILE2}) ||  ( ! -f ${OUTFILE3}) ]] && {
+    [ $1 -eq 0 ] && [[ ( ! -f ${OUTFILE1} ) ||  ( ! -f ${OUTFILE2}) ||  ( ! -f ${OUTFILE3}) ]] && {
         for i in ${OUTFILES[@]}; do
 		testFileExist ${i}
 	done
@@ -61,7 +62,6 @@ testError(){
         BOOLEAN_ERR="true"
     }
 
-    ACTUAL_OUTPUT=$(cat $OUTFILE3 | tr '\n' ' ' )
     if [[ ${ACTUAL_OUTPUT} != *"$WARN"* ]]; then
         MSG=${MSG}"Expected warnings not found.\n"
         echo "$WARN"
@@ -76,13 +76,13 @@ testError(){
         BOOLEAN_ERR="true"
     fi
 
-    testNbLine $1 ${OUTFILE1} ${ACTUAL_NB_LINE_MAP}
-    testNbLine $2 ${OUTFILE2} ${ACTUAL_NB_LINE_ENR}
+    testNbLine $3 ${OUTFILE1} ${ACTUAL_NB_LINE_MAP}
+    testNbLine $4 ${OUTFILE2} ${ACTUAL_NB_LINE_ENR}
 
     #rm $OUTFILE1 $OUTFILE2 $OUTFILE3
     [ ${BOOLEAN_ERR} == "true" ] && {
 	    #echo $MSG
-	    ERRORS=${ERRORS}"\n***************\n##Test \"${TESTS[$4]}\" in $FUNC: \n$MSG"
+	    ERRORS=${ERRORS}"\n***************\n##Test \"${TESTS[$2]}\" in $FUNC: \n$MSG"
 	    return 1
     }
     return 0
@@ -171,19 +171,22 @@ test(){
 
     if [ ! -z $1 ]; then
         if [ $1 == "parameters" ]; then
-            OUTPUT=${OUTPUTS[i]}
+            OUTPUT="${OUTPUTS[i]}"
             NB_LINE_MAP=${NB_LINE_MAPS[i]}
             NB_LINE_ENR=${NB_LINE_ENRS[i]}
         elif [ $1 == "errors" ]; then
+            OUTPUT="${OUTPUTS[i]}"
             EXIT=${EXITS[i]}
-            WARN=${WARNS[i]}
+            WARN="${WARNS[i]}"
         elif [ $1 == "warn" ]; then
-            WARN=${WARNS[i]}
-            OUTPUT=${OUTPUTS[i]}
+            OUTPUT="${OUTPUTS[i]}"
+            WARN="${WARNS[i]}"
+        elif [ $1 == "warn2" ]; then
+            WARN="${WARNS[i]}"
         fi
     fi
 
-        printError ${NB_LINE_MAP} ${NB_LINE_ENR} ${ACTUAL_EXIT} ${i}
+        printError ${ACTUAL_EXIT} ${i} ${NB_LINE_MAP} ${NB_LINE_ENR}
     done
 }
 
@@ -195,10 +198,10 @@ testsDefault(){
     TESTS=('-t 1' '-tEnr 3')
     #TESTS=('' '-t 1' '-tEnr 3' '-s data/recon2.02_without_compartment.xml' '-sep \t' '-sepID ;')
 
-    WARN=${MSG_DEF}
-    OUTPUT=${WARN}" "${MSG_CHECK}
+    WARN="${MSG_DEF}"
+    #fullOutput
+    OUTPUT="${WARN} ${MSG_CHECK}"
     setMappingDefaultParameters
-    #printf "$OUTPUT"
 
     test
 }
@@ -213,7 +216,7 @@ testsMappingDB(){
     MAP_PARS=('14 12.73 0.54' '33 30.0 1.27' '33 30.0 1.27' '4 3.64 0.15' '4 3.64 0.15' '45 40.91 1.74' '14 12.73 0.54' '12 10.91 0.46' '14 12.73 0.54' '13 11.82 0.5' '9 8.18 0.35')
     ENR_PARS=('14 14.43' '38 39.18' '38 39.18' '3 3.09' '3 3.09' '30 30.93' '15 15.46' '13 13.4' '14 14.43' '14 14.43' '10 10.31')
 
-    WARN=${MSG_NAME}
+    WARN="${MSG_NAME}"
     setMultipleOutput
 
     test "parameters"
@@ -228,9 +231,9 @@ testsMass(){
     NB_LINE_ENR='15'
     EXITS=('0' '0' '11' '1' '1' '1')
 
-    local WARN1="[WARNING] Precision has been set without specify isotopic mass column in the fingerprint. [WARNING] By default, it has been set to the 2nd column."${MSG_NAME}
-    local WARN2="Weight precision must be comprise between 1 and 100."${MSG_NAME}
-    WARNS=(${MSG_NAME} ${MSG_NAME} ${WARN1} ${WARN2} ${WARN2} ${WARN2})
+    local WARN1="[WARNING] Weight precision has been set without specify isotopic mass column in the fingerprint. [WARNING] By default, it has been set to the 2nd column."${MSG_NAME}
+    local WARN2="[FATAL] Weight precision must be comprise between 1 and 100."
+    WARNS=("${MSG_NAME}" "${MSG_NAME}" "${WARN1}" "${WARN2}" "${WARN2}" "${WARN2}")
 
     OUTPUTS=()
     OUTPUT=""
@@ -238,7 +241,9 @@ testsMass(){
     setEnrLogDefault "14" "14.43"
     OUTPUTS[0]="$OUTPUT"
     OUTPUTS[1]="$OUTPUT"
-    OUTPUTS[2]="[FATAL] All the values of the selected database(s) are badly formatted. Please check the column number set for these databases. -noCheck to ignore the bad format exit and run the analysis anyway."
+    #fullOutput
+    OUTPUTS[2]="${WARN1} [FATAL] All the values of the selected database(s) are badly formatted. Please check the column number set for these databases. -noCheck to ignore the bad format exit and run the analysis anyway."
+    OUTPUTS[3]="${WARN2}"
 
     test "errors"
 }
@@ -247,10 +252,14 @@ testsID_SBML(){
     setUp
 
     TESTS=('' '-idSBML 2' '-idSBML -2')
+    WARNS=("${MSG_DEF}" "${MSG_NAME}" "[WARNING] -idSBML column parameter must be positive. ${MSG_NEG_MAP}${MSG_NAME}")
 
-    WARNS=(${MSG_DEF} ${MSG_NAME} '[WARNING] -idSBML column parameter must be positive.\n'${MSG_NEG_MAP}${MSG_NAME})
-    OUTPUT=${MSG_CHECK}
-    setMappingDefaultParameters
+    #fullOutput
+    for i in `seq 0 $((${#TESTS[@]} -1))`; do
+        OUTPUT="${WARNS[i]} ${MSG_CHECK}"
+        setMappingDefaultParameters
+        OUTPUTS[i]="$OUTPUT"
+    done
 
     test "warn"
 }
@@ -261,9 +270,44 @@ testsInChILayers(){
     TESTS=('-l chp' '-l c,hp' '-l c,' '-l xertg')
     WARN="-l parameter badly formatted: it must be a list containing the number - separated by comma without blank spaces - of the InChi's layer concerned by the mapping (by default: c,h; for a mapping including all the layers, enter c,h,q,p,b,t,i,f,r; for a mapping on formula layer only, enter the -l option with no parameter)"
     OUTPUT=""
-    EXIT='1'
+    EXIT=1
 
     test
+}
+
+testsName(){
+    setUp
+
+    TESTS=('-nameCol 1' '-nameCol 1 -name 0')
+    WARN="$MSG_MAP"
+    OUTPUT="${WARN} ${MSG_CHECK}"
+    setMappingDefaultParameters
+
+    test
+}
+
+testsNameMap(){
+    setUp
+
+    TESTS=('-nameCol -2 -name 1' 'name 1' '-name 1 -nameCol 2')
+    NB_LINE_MAP=11
+    NB_LINE_ENR=6
+    WARNS=('' '' "[WARNING] You have set both name column and name mapping parameters and with different parameters.\n[WARNING] By default, the name mapping is activated with the column number of this parameter.\n")
+    OUTPUT=""
+    #setMapLogDefault "50" "45.45" "1.93"
+    #setEnrLogDefault "14" "14.43"
+
+    test "warn2"
+}
+
+testsNameNegative(){
+    setUp
+
+    TESTS=('-name -1' '-nameCol -1 -name -1' '-nameCol -1' '-name -1' '-nameCol -1')
+    MSG2=($MSG_NEG_MAP $MSG_NEG_MAP $MSG_MAP $MSG_MAP)
+    MSG=MSG2'[WARNING] Your column number for the name of the chemicals is negative'$WARN_NAME
+    NB_LINE_MAP=''
+    NB_LINE_ENR=''
 }
 
 ########### MAIN ###########
@@ -274,8 +318,8 @@ printf "Tests in progress, could take a few minutes...\n"
 
 #testsDefault
 #testsMappingDB
-testsMass
-#testsID_SBML
+#testsMass
+testsID_SBML
 #testsInChILayers
 
 #rm -r temp/
